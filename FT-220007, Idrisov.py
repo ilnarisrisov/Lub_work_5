0eb3b943d55b3721ae7c5020723c4cdf7fdb9b88
ilnarisrisov <146421910+ilnarisrisov@users.noreply.github.com>
2023-10-28 21:00:36 +0500

    Update FT-220007, Idrisov.py
    Добавил возможность шифровать русскими буквами

--- FT-220007, Idrisov.py
+++ FT-220007, Idrisov.py
@@ -1,24 +1,53 @@
-sentence = str(input('\nВведите предложение, написанное латинскими буквами: '))
+language = input('Введите "1", если хотите зашифровать код на русском языке'
+                     ', введите "2", если хотите зашифровать на латинском: ')
+sentence = str(input('\nВведите предложение: '))
 shift = int(input('\nВведите сдвиг шифра Цезаря: '))
 
-if shift > 0 and shift < 27:
 
+def Ceasar_s_cipher_RU(sentence, shift):
 
-    def Ceasar_s_cipher(sentence, shift):
+    encrypted_sentence = ''
 
-        encrypted_sentence = ''
-        for char in sentence:
-            if char.isalpha():
-                ascii_offset = ord('a') if char.islower() else ord('A')
-                encrypted_char = chr((ord(char) - ascii_offset + shift) % 26 + ascii_offset)
+    i = 0
+    while i < len(sentence):
+        letter = sentence[i]
+        if letter.isalpha():
+            if letter.isupper():
+                encrypted_sentence += chr((ord(letter) - 1040 + shift) % 33 + 1040)
+
             else:
-                    encrypted_char = char
-            encrypted_sentence += encrypted_char
-        return encrypted_sentence
+                encrypted_sentence += chr((ord(letter) - 1072 + shift) % 33 + 1072)
+
+        else:
+            encrypted_sentence += letter
+
+        i += 1
+    return encrypted_sentence
 
 
-    encrypted_sentence = Ceasar_s_cipher(sentence, shift)
-    print('\nЗашифрованный текст: ' + encrypted_sentence)
+def Ceasar_s_cipher_EN(sentence, shift):
+
+    encrypted_sentence = ''
+
+    for letter in sentence:
+        if letter.isalpha():
+            ascii_offset = ord('a') if letter.islower() else ord('A')
+            encrypted_letter = chr((ord(letter) - ascii_offset + shift) % 26 + ascii_offset)
+        else:
+            encrypted_letter = letter
+        encrypted_sentence += encrypted_letter
+    return encrypted_sentence
+
+if language == '1':
+
+    encrypted_sentence = Ceasar_s_cipher_RU(sentence, shift)
+    print('\nЗашифрованный текст: ' + str(encrypted_sentence))
+
+elif language == '2':
+
+    encrypted_sentence = Ceasar_s_cipher_EN(sentence, shift)
+    print('\nЗашифрованный текст: ' + str(encrypted_sentence))
 
 else:
-    print('\nВыбран слишком большой сдвиг шифра')
+    print('\nВведено неверное число для выбора языка')
+
